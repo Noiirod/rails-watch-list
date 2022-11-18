cd0baed0bb9b9416138b144a0e7316fcce446967
steven dorion <noirod.steven@gmail.com>
2022-11-18 12:27:42 +0100

    create app on heroku

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[new create]
+  before_action :set_list, only: %i[new create destroy]
   def new
     @movies = Movie.all
     @bookmark = Bookmark.new
